fe843ab40a2121911264429b85a50694572d9f2d
Lucas Aster <asterlc102@gmail.com>
2022-02-12 14:08:19 -0300

    map required data

--- controllers/breedsController.js
+++ controllers/breedsController.js
@@ -3,7 +3,21 @@ const api = require('../api/api');
 exports.getBreeds = async (req, res, next) => {
     try {
         const { data } = await api.get('breeds');
-        return res.status(200).send({ Breeds: data });
+        const breedInfo = data.map(el => {
+            return {
+                name: el.name,
+                origin: el.origin,
+                bredFor: el.bredFor,
+                weight: {
+                    imperial: el.weight.imperial,
+                    metric: el.weight.metric
+                },
+                group: el.breedGroup,
+                lifeSpan: el.life_span,
+                image: el.image.url
+            }
+        });
+        return res.status(200).send({ Breeds: breedInfo });
     } catch (error) {
         throw res.status(400).send({ Error: error.message });
     }
